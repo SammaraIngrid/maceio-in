1283d89ce91c001ea1c0ff0c6a71e417d9fb64e9
Sammara Ingrid <sammaracavalcante2@gmail.com>
2025-02-11 16:33:43 -0300

    Criando models e migrations para Employees e Users

---
+++ db/migrate/20250211192223_create_employees.rb
@@ -0,0 +1,12 @@
+class CreateEmployees < ActiveRecord::Migration[7.1]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :department
+      t.string :email
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
