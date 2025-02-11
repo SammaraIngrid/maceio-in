c41a80a7718242e6647c599fa06fc65aba2320ed
Sammara Ingrid <sammaracavalcante2@gmail.com>
2025-02-11 17:34:51 -0300

    Remove gem ransack

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,5 @@
 class EmployeesController < ApplicationController
   def index
-    @query = Employee.all.order(created_at: :desc).ransack(params[:q])
-
-    @employee = @query.result
+    @query = Employee.all.order(created_at: :desc)
   end
 end
