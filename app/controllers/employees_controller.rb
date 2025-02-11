b05d6457db49038c471ccfa1c5f6e651fff51d29
Sammara Ingrid <sammaracavalcante2@gmail.com>
2025-02-11 18:09:45 -0300

    Adicionadas tabela e colunas de funcionarios no Index

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
   def index
-    @query = Employee.all.order(created_at: :desc)
+    @employees = Employee.all.order(created_at: :desc)
   end
 end
