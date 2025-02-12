f042fc4588320287767eb709f44d3d1995e96b82
Sammara Ingrid <sammaracavalcante2@gmail.com>
2025-02-12 16:25:40 -0300

    Adicionada a traducao e selecao dos setores de Employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,11 @@
 class Employee < ApplicationRecord
   belongs_to :user
   
-  as_enum :department, [:accounting, :finance, :customer_service, :budgeting, :it ],  prefix: true, map: :string
+  as_enum :department, [:accounting, :finance, :customer_service, :budgeting, :it],  prefix: true, map: :string
+
+  def translated_department
+    return if department.blank?
+
+    I18n.t("activerecord.attributes.employee.department.#{department}")
+  end
 end
