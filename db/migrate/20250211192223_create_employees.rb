6790e9ada460a88768d43d20fe45af77369bec82
Sammara Ingrid <132792505+SammaraIngrid@users.noreply.github.com>
2025-02-12 13:51:42 -0300

    Adicionado ENUM no campo department (#3)

--- db/migrate/20250211192223_create_employees.rb
+++ db/migrate/20250211192223_create_employees.rb
@@ -2,7 +2,7 @@ class CreateEmployees < ActiveRecord::Migration[7.1]
   def change
     create_table :employees do |t|
       t.string :name
-      t.string :department
+      t.string :department_cd
       t.string :email
       t.references :user, null: false, foreign_key: true
 
